9ac767e9d9993b4b13d058cc1d096ea63e3d937a
Emory Merryman <emory.merryman@gmail.com>
2016-05-15 23:47:08 +0000

    crazy

--- init.sh
+++ init.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-mkdir /tmp/gpg-agent &&
-    chmod 700 /tmp/gpg-agent &&
-    eval $(gpg-agent --write-env-file /tmp/gpg-agent/gpg_agent_info \
-                     --use-standard-socket --daemon \
-                     --default-cache-ttl=${GPG_DEFAULT_CACHE:-31536000} \
-                     --max-cache-ttl=${GPG_MAX_CACHE:-31536000} ) &&
-    while gpg-connect-agent /bye
-    do
-	sleep 2 &&
-	    true
-    done &&
-    cp /home/${LUSER}/private/id_rsa /home/${LUSER}/.ssh/id_rsa &&
+cp /home/${LUSER}/private/id_rsa /home/${LUSER}/.ssh/id_rsa &&
     chmod 0600 /home/${LUSER}/.ssh/id_rsa &&
     (gpg --allow-secret-key- --import /home/${LUSER}/private/secret.key || true ) &&
     (gpg2 --allow-secret-key- --import /home/${LUSER}/private/secret.key || true ) &&
