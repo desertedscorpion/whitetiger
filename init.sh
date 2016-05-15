b3bb51645ae65b318e6a71d8e00312cb52c0385b
Emory Merryman <emory.merryman@gmail.com>
2016-05-15 21:20:55 +0000

    reorder

--- init.sh
+++ init.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-cp /home/${LUSER}/private/id_rsa /home/${LUSER}/.ssh/id_rsa &&
+mkdir /tmp/gpg-agent &&
+    chmod 700 /tmp/gpg-agent &&
+    eval $(gpg-agent --write-env-file /tmp/gpg-agent/gpg_agent_info \
+                     --use-standard-socket --daemon \
+                     --default-cache-ttl=${GPG_DEFAULT_CACHE:-31536000} \
+                     --max-cache-ttl=${GPG_MAX_CACHE:-31536000} ) &&
+    while gpg-connect-agent /bye
+    do
+	sleep 2 &&
+	    true
+    done &&
+    cp /home/${LUSER}/private/id_rsa /home/${LUSER}/.ssh/id_rsa &&
     chmod 0600 /home/${LUSER}/.ssh/id_rsa &&
     (gpg --allow-secret-key- --import /home/${LUSER}/private/secret.key || true ) &&
     (gpg2 --allow-secret-key- --import /home/${LUSER}/private/secret.key || true ) &&
@@ -14,17 +25,6 @@ cp /home/${LUSER}/private/id_rsa /home/${LUSER}/.ssh/id_rsa &&
     pass git checkout master &&
     pass git rebase origin/master &&
     ln --symbolic --force /home/${LUSER}/bin/post-commit /home/${LUSER}/.password-store/.git/hooks &&
-    mkdir /tmp/gpg-agent &&
-    chmod 700 /tmp/gpg-agent &&
-    eval $(gpg-agent --write-env-file /tmp/gpg-agent/gpg_agent_info \
-                     --use-standard-socket --daemon \
-                     --default-cache-ttl=${GPG_DEFAULT_CACHE:-31536000} \
-                     --max-cache-ttl=${GPG_MAX_CACHE:-31536000} ) &&
-    while gpg-connect-agent /bye
-    do
-	sleep 2 &&
-	    true
-    done &&
     /usr/bin/bash &&
     /home/${LUSER}/bin/post-commit &&
     true
